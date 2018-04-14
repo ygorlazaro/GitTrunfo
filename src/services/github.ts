fb6f77d9bead93df8577871aaab5c8ad17ac416c
Ygor Lazaro <ygor@ygorlazaro.com>
2018-04-14 17:44:04 -0300

    Adding typescript

--- src/services/github.ts
+++ src/services/github.ts
@@ -1,20 +1,20 @@
 import axios from 'axios';
 import queryString from 'query-string'
 
-const getQueryString = () => {
+const getQueryString = (): string => {
     const params = {
         client_id: '7527375cd264c8530f30',
         client_secret: '8675345800219782348c21267e31d18b15c5aea3'
-    }
+    };
 
     return '?' + queryString.stringify(params);
 }
 
-const url = 'https://api.github.com/';
+const url: string = 'https://api.github.com/';
 
 class GitHubService {
 
-    getUser(username) {
+    getUser(username: string) {
         return axios.get(`${url}users/${username}${getQueryString()}`)
     }
 
