a0063da5000a0d259c638ede82afcea5fe3b92ba
Ygor Lazaro <ygor@ygorlazaro.com>
2019-05-29 19:09:16 -0300

    [Title]

--- src/components/GameRules.vue
+++ src/components/GameRules.vue
@@ -1,16 +1,16 @@
 <template>
-    <section class="section">
-      <div class="container">
-        <h1 class="title">Regras</h1>
-        <h2 class="subtitle">
-         <p>Esse é um joguinho simples. Se você já jogou SuperTrunfo, já sabe as regras.</p>
-         <p>Caso não tenha jogado, basta escolher uma carta da sua lista e jogar contra a carta escolhida pelo sistema. Você ganha se tiver um valor maior no atributo escolhido.</p>
-        </h2>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Regras do Jogo</h1>
+      <h2 class="subtitle">
+        <p>Esse é um joguinho simples. Se você já jogou SuperTrunfo, já sabe as regras.</p>
+        <p>Caso não tenha jogado, basta escolher uma carta da sua lista e jogar contra a carta escolhida pelo sistema. Você ganha se tiver um valor maior no atributo escolhido.</p>
+      </h2>
 
-        <router-link to="/game" class="btn btn-primary">
-          <img src="../assets/github.png" alt="GitHub Logo" width="32px" height="32px">
-          Entre com seu GitHub para começar
-        </router-link>
-      </div>
-    </section>
+      <router-link to="/game" class="btn btn-primary">
+        <img src="../assets/github.png" alt="GitHub Logo" width="32px" height="32px">
+        Entre com seu GitHub para começar
+      </router-link>
+    </div>
+  </section>
 </template>
